d9f7382c6d2e428530bf44825e79b0e782459afe
sleektimmy <sleektimmy@gmail.com>
2025-06-25 05:48:22 +0100

    feat: Update job listings and enhance navigation with categories

--- src/app/components/navigation/Mobile.jsx
+++ src/app/components/navigation/Mobile.jsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import NavData from "./NavData";
+import Image from "next/image";
 
 export default function Mobile() {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,12 +14,12 @@ export default function Mobile() {
     }
     return (
         <>
-        <div className="md:hidden flex justify-between items-center bg-orange-600 text-slate-50 h-16 px-4">
-            <Link href="/" className="text-2xl font-bold">HR Platform</Link>
+        <div className="md:hidden flex justify-between items-center bg-red-800 text-slate-50 h-16 px-4">
+            <Link href="/" className="text-2xl font-bold"><Image src="/logo.png" alt="HR Platform" width={50} height={50} className="rounded-full bg-white"/></Link>
             <GiHamburgerMenu className="text-3xl cursor-pointer" onClick={handleNavToggle} />
         </div>
         {isOpen && (
-            <ul className="flex space-x-2 flex-col md:hidden justify-around items-center text-slate-50 bg-orange-600 h-screen font-semibold text-lg capitalize "
+            <ul className="flex space-x-2 flex-col md:hidden justify-around items-center text-slate-50 bg-red-800 h-screen font-semibold text-lg capitalize "
             onClick={() => setIsOpen(false)}>
                 <NavData />
             </ul>
